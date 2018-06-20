de38b0a20171fe8376714a175b8680e98768617f
rohan_vishnoi <avirohanvishnoi@gmail.com>
2018-06-21 00:40:33 +0530

    00:40

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,8 +8,36 @@ namespace ConsoleApplication1
 {
     class Program
     {
+
+        static int area(int h, int w)
+        {
+            return h * w;
+        }
+        static int pow(int x, int y = 2)
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+        static int sum(int x, int y)
+        {
+           
+           return x + y;
+            
+        }
+            
         static void Main(string[] args)
         {
+            int res = sum(12, 19);
+            Console.WriteLine(res);
+            Console.WriteLine(pow(3,40));
+            Console.WriteLine(area(12, 17));
+           Console.ReadKey();
         }
+        
+       }
     }
-}
+
